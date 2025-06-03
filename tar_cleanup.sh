26f981c1f9cb2d5721248505d5e1b624890b10a4
LinuxSystemsEngineer <111530676+LinuxSystemsEngineer@users.noreply.github.com>
2025-06-03 12:34:28 -0500

    localhost: updated the tar_cleanup.sh Linux bash script
    localhost: updated the tar_cleanup.sh Linux bash script

--- tar_cleanup.sh
+++ tar_cleanup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # Log file path
-logfile="./log_cleanup.log"
+logfile="/home/<username>/backups/log_cleanup.log"
 touch "$logfile"
 chmod 664 "$logfile"
 
 # Log the start of the cleanup
 echo "$(date +"%Y-%m-%d %H:%M:%S") Cleanup started" >> "$logfile"
 
-# Find and remove backups older than 3 days, log number of deleted files
-deleted_files=$(sudo find ./ -type f -mtime +3 -name '*-full-tar-backup.tar.gz' -exec rm -rfv {} \; | tee -a "$logfile" | wc -l)
+# Find and remove backups created more than 3 days ago, log number of deleted files
+deleted_files=$(sudo find ./ -type f -name "*.tar.gz" -exec stat -c "%W %n" {} \; | awk '$1 != 0 && (systime() - $1) > 242200 {print $2}' | xargs -I {} sudo rm -rfv {} | tee -a "$logfile" | wc -l)
 
 # Log cleanup completion with the number of deleted files
-echo "$(date +"%Y-%m-%d %H:%M:%S") Cleanup completed - $deleted_files files deleted" >> "$logfile"
-
+echo "$(date +"%Y-%m-%d %H:%M:%S") Cleanup completed = $deleted_files files deleted" >> "$logfile"
